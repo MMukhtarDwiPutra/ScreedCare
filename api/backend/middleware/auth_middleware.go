0e53a2c91ee866e1254fa1a5132ba29024d58260
MMukhtarDwiPutra <putra16.mp@gmail.com>
2022-08-13 03:01:34 +0700

    Update auth middleware

---
+++ api/backend/middleware/auth_middleware.go
@@ -0,0 +1,28 @@
+package middleware
+
+import(
+	"net/http"
+	"ScreedCare/model"
+	"encoding/json"
+	"fmt"
+)
+
+func AuthMiddleware(w http.ResponseWriter, r *http.Request) bool{
+	if "RAHASIA" == r.Header.Get("X-API-Key") {
+		// ok
+		return true
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnauthorized)
+
+		webResponse := model.WebResponse{
+			Code:   http.StatusUnauthorized,
+			Status: "UNAUTHORIZED",
+		}
+
+		byteData, _ := json.Marshal(&webResponse)
+		w.Header().Add("Content-type", "application/json")
+		w.Write(byteData)
+		return false
+	}
+}
