2930cc185343dafa774fb579c5623f22b76b8968
MMukhtarDwiPutra <putra16.mp@gmail.com>
2022-09-08 19:00:24 +0700

    Update App.js

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,10 +1,14 @@
 
 import Navigation from "./components/Navigation.js";
 import Maincontent from "./components/Maincontent.js";
+import MetaTags from "react-meta-tags";
 
 function App() {
   return (
     <>
+      <MetaTags>
+      <meta name="google-site-verification" content="VIUZDNIlJdjy1bIIb2wi8wYKIxpCx_12OZHM7eUikCE" />
+      </MetaTags>
       <Navigation />
       <Maincontent />
     </>
